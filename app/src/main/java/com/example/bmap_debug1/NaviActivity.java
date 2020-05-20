8c62bc20c176ff548c9d7e2ae03b387e1f1252ea
VasylHsiao <61651192+VasylHsiao@users.noreply.github.com>
2020-05-20 20:57:03 +0800

    2
    2

--- app/src/main/java/com/example/bmap_debug1/NaviActivity.java
+++ app/src/main/java/com/example/bmap_debug1/NaviActivity.java
@@ -1,13 +1,16 @@
 package com.example.bmap_debug1;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NaviActivity extends AppCompatActivity {
+public class NaviActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi);
     }
+
+
 }
